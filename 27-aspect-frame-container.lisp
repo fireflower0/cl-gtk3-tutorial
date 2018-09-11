1bdb796e2e2effb005be16516301fa834224a0d6
fireflower0 <fireflower0@hotmail.co.jp>
2018-09-12 07:35:56 +0900

    Add an aspect frame container program

---
+++ 27-aspect-frame-container.lisp
@@ -0,0 +1,37 @@
+;; 27 アスペクトフレームコンテナ
+
+;; ライブラリロード
+(ql:quickload :cl-cffi-gtk)
+
+(defun main ()
+  (gtk:within-main-loop
+   (let ((window (make-instance 'gtk:gtk-window
+                                :type :toplevel
+                                :title "Aspect Frame"
+                                :default-width 300
+                                :default-height 250
+                                :border-width 12))
+         (frame  (make-instance 'gtk:gtk-aspect-frame
+                                :label "Ratio 2 x 1"
+                                :xalign 0.5
+                                :yalign 0.5
+                                :ratio 2
+                                :obey-child nil))
+         (area   (make-instance 'gtk:gtk-drawing-area
+                                :width-request 200
+                                :hight-request 200)))
+
+     ;; destroy シグナルをラムダ関数と結びつける
+     (gobject:g-signal-connect window "destroy"
+                               (lambda (widget)
+                                 (declare (ignore widget))
+                                 (gtk:leave-gtk-main)))
+     
+     (gtk:gtk-container-add window frame)
+     (gtk:gtk-container-add frame area)
+
+     ;; ウィジェット表示
+     (gtk:gtk-widget-show-all window))))
+
+;; main関数を呼び出して実行
+(main)
