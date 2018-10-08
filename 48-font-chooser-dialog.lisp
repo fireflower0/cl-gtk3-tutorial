b15bcba8bc3bff1f89e5f1dd4dc0df809e793984
fireflower0 <fireflower0@hotmail.co.jp>
2018-10-08 10:08:17 +0900

    Add a file chooser dialog program

---
+++ 48-font-chooser-dialog.lisp
@@ -0,0 +1,52 @@
+;; 48 フォント選択ダイアログ
+
+;; ライブラリロード
+(ql:quickload :cl-cffi-gtk)
+
+(defun font-filter (family face)
+  (declare (ignore face))
+  (member (pango:pango-font-family-get-name family)
+          '("Sans" "Serif")
+          :test #'equal))
+
+(defun main ()
+  (gtk:within-main-loop
+    (let ((window (make-instance 'gtk:gtk-window
+                                 :title "Font Chooser Button"
+                                 :type :toplevel
+                                 :border-width 12
+                                 :default-width 300
+                                 :default-height 100))
+          (button (make-instance 'gtk:gtk-font-button)))
+
+      ;; destroy シグナルをラムダ関数と結びつける
+      (gobject:g-signal-connect window "destroy"
+                                (lambda (widget)
+                                  (declare (ignore widget))
+                                  (gtk:leave-gtk-main)))
+
+      ;; フォントチューザーのフォントを選択するフィルター機能を設定する
+      (gtk:gtk-font-chooser-set-filter-func button #'font-filter)
+      (gobject:g-signal-connect button "font-set"
+                                (lambda (widget)
+                                  (declare (ignore widget))
+                                  (format t "Font is set:~%")
+                                  (format t "   Font name   : ~A~%"
+                                          (gtk:gtk-font-chooser-get-font button))
+                                  (format t "   Font family : ~A~%"
+                                          (pango:pango-font-family-get-name
+                                           (gtk:gtk-font-chooser-get-font-family button)))
+                                  (format t "   Font face   : ~A~%"
+                                          (pango:pango-font-face-get-face-name
+                                           (gtk:gtk-font-chooser-get-font-face button)))
+                                  (format t "   Font size   : ~A~%"
+                                          (gtk:gtk-font-chooser-get-font-size button))))
+
+      ;; ボタンをウィンドウに追加
+      (gtk:gtk-container-add window button)
+
+      ;; ウィジェット表示
+      (gtk:gtk-widget-show-all window))))
+
+;; main関数を呼び出して実行
+(main)
