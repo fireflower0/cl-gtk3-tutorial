36f5d4f9ecb1a662500d0dcefbe5a7f1336913f3
fireflower0 <fireflower0@hotmail.co.jp>
2018-10-22 07:15:03 +0900

    Fix cairo clock demo program

--- 60-demo-cairo-clock.lisp
+++ 60-demo-cairo-clock.lisp
@@ -15,7 +15,27 @@
                         (setf (egg-clock-face-time clock)
                               (multiple-value-list (get-decoded-time)))
                         (gtk:gtk-widget-queue-draw clock)
-                        glib:+g-source-continue+)))
+                        glib:+g-source-continue+))
+  ;; 時計を描画するシグナルハンドラ
+  (gobject:g-signal-connect clock "draw"
+                            (lambda (widget cr)
+                              (let ((cr (gobject:pointer cr))
+                                    ;; ウィジェットのGdkWindowを取得する
+                                    (window (gtk:gtk-widget-window widget)))
+                                ;; surfaceクリア
+                                (cairo:cairo-set-source-rgb cr 1.0 1.0 1.0)
+                                (cairo:cairo-paint cr)
+                                (let* ((x (/ (gdk:gdk-window-get-width  window) 2))
+                                       (y (/ (gdk:gdk-window-get-height window) 2))
+                                       (radius (- (min x y) 12)))
+                                  ;; 時計背景
+                                  (cairo:cairo-arc cr x y radius 0 (* 2 pi))
+                                  (cairo:cairo-set-source-rgb cr 1 1 1)
+                                  (cairo:cairo-fill-preserve cr)
+                                  (cairo:cairo-set-source-rgb cr 0 0 0)
+                                  (cairo:cairo-stroke cr)
+                                  ;; 時計針
+                                  )))))
 
 (defun main ()
   (gtk:within-main-loop
