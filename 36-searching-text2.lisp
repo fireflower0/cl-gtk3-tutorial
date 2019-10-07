172155c2591ad1624c80d4f7b8d77eb64c39ab7e
fireflower0 <fireflower0@hotmail.co.jp>
2019-10-08 00:17:10 +0900

    Fixed a 36

--- 36-searching-text2.lisp
+++ 36-searching-text2.lisp
@@ -1,7 +1,9 @@
 ;; 36 テキストビューでテキストを検索2
 
-;; ライブラリロード
-(ql:quickload :cl-cffi-gtk)
+(defpackage #:cl-gtk3-tutorial/36-searching-text2
+  (:use #:cl)
+  (:export #:main))
+(in-package #:cl-gtk3-tutorial/36-searching-text2)
 
 (defvar *some-text* "こんにちは、世界！")
 
@@ -61,7 +63,7 @@
                                                  (gtk:gtk-text-buffer-get-iter-at-mark buffer
                                                                                    last-pos))))))
       
-      (gtk:gtk-text-buffer-set-text (gtk:gtk-text-view-buffer text-view) *some-text*)
+      (gtk:gtk-text-buffer-insert (gtk:gtk-text-view-buffer text-view) *some-text*)
       
       (gtk:gtk-container-add scrolled text-view)
       (gtk:gtk-container-add hbox entry)
@@ -75,6 +77,3 @@
 
       ;; ウィジェット表示
       (gtk:gtk-widget-show-all window))))
-
-;; main関数を呼び出して実行
-(main)
